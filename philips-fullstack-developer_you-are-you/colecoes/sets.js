244da57314124f8c71e915bea0364cfe2b40161a
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-03-25 09:06:19 -0300

    Atualizando o arquivo sets.js

--- philips-fullstack-developer_you-are-you/colecoes/sets.js
+++ philips-fullstack-developer_you-are-you/colecoes/sets.js
@@ -2,6 +2,9 @@ const arr = [30, 30, 40, 5, 223, 2049, 3034, 5];
 
 function uniqueElements(array) {
 	let unique = new Set(array);
+    // Também pode ser
+    // const unique = new Set(array);
+
 	return [...unique];
 }
 

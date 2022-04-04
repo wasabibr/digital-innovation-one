5f01b72ee20f1c817570df5fbaa26bd1ffba33f1
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-04-04 19:49:25 -0300

    Inserindo o arquivo desafio1-soma-simples.js

---
+++ orbi-web-games-developer/desafios-iniciais-javascript_-_orbi-web-game/desafio1-soma-simples.js
@@ -0,0 +1,7 @@
+let A = parseInt(gets());
+let B = parseInt(gets());
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+let SOMA = A + B;               
+print("SOMA = "+ SOMA );

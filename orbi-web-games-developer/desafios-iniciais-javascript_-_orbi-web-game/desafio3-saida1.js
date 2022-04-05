f0aa17e591b48c98aa40e0703a4728eece66b5d8
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-04-04 22:30:33 -0300

    Inserindo o arquivo desafio3-saida1.js

---
+++ orbi-web-games-developer/desafios-iniciais-javascript_-_orbi-web-game/desafio3-saida1.js
@@ -0,0 +1,11 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+print('---------------------------------------');
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+for(let i = 0; i < 5; i++){
+  print('|                                     |');
+}
+print('---------------------------------------');

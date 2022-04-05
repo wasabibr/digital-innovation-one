200261c3f36642222e8fc6efc1c3e336fb7d76c8
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-04-05 13:25:39 -0300

    Inserindo arquivo desafio2-blobs.js

--- impulso-fullstack-web-developer/desafios-impulso-iniciante_- _javascript/desafio2-blobs.js
+++ impulso-fullstack-web-developer/desafios-impulso-iniciante_- _javascript/desafio2-blobs.js
@@ -2,12 +2,11 @@ var n = parseInt(gets());
 
 //implemente sua solução nos espaços em branco
 
-while (n >=  1     && n <=    1000    ) {
+while (n >= 1 && n <= 1000) {
   var c = parseFloat(gets());
-  
-  var dias =  0   ;
-  while ( c >=   1    && c <=    1000    ) {
-    c = c / 2.0;
+  var dias =0 ;
+  while (c > 1 && c <= 1000) {
+    c /= 2.0;
     dias++;
   }
   n--;

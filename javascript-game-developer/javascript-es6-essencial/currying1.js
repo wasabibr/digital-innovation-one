dfc6b2c326de31568e061453c9b30970868a9ff4
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-04-09 20:24:16 -0300

    Atualizando o arquivo currying1.js

--- javascript-game-developer/javascript-es6-essencial/currying1.js
+++ javascript-game-developer/javascript-es6-essencial/currying1.js
@@ -2,7 +2,7 @@ function soma(a, b) {
     return a + b;
 }
 
-soma(2, 2);
-soma(2, 3);
-soma(2, 4);
-soma(2, 5);
+console.log(soma(2, 2));
+console.log(soma(2, 3));
+console.log(soma(2, 4));
+console.log(soma(2, 5));

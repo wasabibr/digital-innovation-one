4ea6e8ea9ffeed2b4835ad186006c345c1f7a827
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-04-10 19:23:18 -0300

    Atualizando o arquivo scripts.js

--- philips-fullstack-developer_you-are-you/javascript-assincrono/assets/js/scripts.js
+++ philips-fullstack-developer_you-are-you/javascript-assincrono/assets/js/scripts.js
@@ -1,3 +1,54 @@
+// Primeira forma
+
+/*
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
+
+
+const getCats = async () => {
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+		return json.webpurl;
+	} catch (e) {
+		console.log(e.message);
+	}
+};
+
+const loadImg = async () => {
+	const catImg = document.getElementById('cat');
+	catImg.src = await getCats();
+};
+
+catBtn.addEventListener('click', loadImg);
+
+loadImg();
+*/
+
+// Segunda forma
+
+/*
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
+
+const getCats = async () => {
+	const data = await fetch(BASE_URL);
+		.then((res) => res.json());
+		.catch((e) => console.log(e));
+
+	return data.webpurl;
+};
+
+const loadImg = async () => {
+	const catImg = document.getElementById('cat');
+	catImg.src = await getCats();
+};
+
+catBtn.addEventListener('click', loadImg);
+
+loadImg();
+*/
+
 const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
 
 const getCats = async () => {

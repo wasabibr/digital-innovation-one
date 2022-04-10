a8ce8a15dd509c6cf33e0cc7d10c7ea4fa29ea40
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-04-09 22:06:55 -0300

    Inserindo o arquivo variaveis1.js

---
+++ javascript-game-developer/javascript-es6-essencial/variaveis1.js
@@ -0,0 +1,7 @@
+var nameVar = 'Jefferson';
+let nameLet = 'Jefferson';
+const nameConst = 'Jefferson';
+
+console.log(`nameVar: ${nameVar}`);
+console.log(`nameLet: ${nameLet}`);
+console.log(`nameConst: ${nameConst}`);

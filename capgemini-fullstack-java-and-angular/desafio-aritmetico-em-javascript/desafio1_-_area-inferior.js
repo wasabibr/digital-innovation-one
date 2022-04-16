7d4972fda8b494a7601eec20c35e5ad7b7d376a9
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-04-16 11:37:59 -0300

    Iniciando o diretório desafio-aritmetico-em-javascript e inserindo o arquivo desafio1_-_area-inferior.js

---
+++ capgemini-fullstack-java-and-angular/desafio-aritmetico-em-javascript/desafio1_-_area-inferior.js
@@ -0,0 +1,109 @@
+const operacao = gets();
+
+const isSum = operacao === "S";
+
+
+
+const xL = 12, yL = 12;
+
+let sum = 0.0, av = 0.0;
+
+
+
+let linha = gets();
+
+
+
+if (linha.length > 5) {
+
+  for (var y = 0; y < yL; y++) {
+
+
+
+    if (y != 0) {
+
+      linha = gets();
+
+    }
+
+    let numeros = linha.trim().split(/[\s,]+/);
+
+
+
+    for (var x = 0; x < xL; x++) {
+
+
+
+      if ((y > 6) && (x < y) && (x > 11 - y)) {
+
+        var valor = Number(numeros[x]);
+
+        sum += valor;
+
+        ++av;
+
+      }
+
+    }
+
+  }
+
+}
+
+else {
+
+
+
+  var contagem = 1;
+
+  for (var y = 0; y < yL; y++) {
+
+
+
+    for (var x = 0; x < xL; x++) {
+
+
+
+      var valor = 0;
+
+
+
+      if (contagem == 1) {
+
+        valor = Number(linha);
+
+        primeiroloop = false;
+
+      }
+
+      else {
+
+        linha = gets().trim();
+
+        valor = Number(linha);
+
+      }
+
+
+
+      if ((y > 6) && (x < y) && (x > 11 - y)) {
+
+        sum += valor;
+
+        ++av;
+
+      }
+
+
+
+      contagem++
+
+    }
+
+  }
+
+}
+
+
+
+console.log((sum / (isSum ? 1 : av)).toFixed("1"));
